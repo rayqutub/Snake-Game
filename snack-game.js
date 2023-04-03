68371b0e9ca72146e65598114eae922d7c32ff3c
Rehaan Qutub <83799911+rayqutub@users.noreply.github.com>
2023-04-02 18:41:21 -0700

    Add files via upload

---
+++ snack-game.js
@@ -0,0 +1,124 @@
+const grid = document.querySelector(".grid")
+const button = document.getElementById("start")
+const scoreDisplay = document.getElementById("score")
+let squares = []
+let currentSnake = [2,1,0]
+let direction = 1
+let width = 10
+let appleIndex = 0
+let score = 0
+let intervalTIme = 1000
+let speed = 0.9
+let timerId = 0
+
+
+function createGrid(){
+    for (let i = 0; i < 100; i++) {
+
+    const square = document.createElement("div")
+    
+    square.classList.add("square")
+    grid.appendChild(square)
+
+    squares.push(square)
+
+    }
+    
+    
+
+}
+createGrid()
+currentSnake.forEach(index => squares[index].classList.add("snake"))
+
+function startButton(){
+    currentSnake.forEach(index => squares[index].classList.remove("snake"))
+    squares[appleIndex].classList.remove("apple")
+    clearInterval(timerId)
+    currentSnake = [2,1,0]
+    score = 0
+    scoreDisplay.textContent = score
+    direction = 1
+    intervalTIme = 1000
+    generateApples()
+    currentSnake.forEach(index => squares[index].classList.add("snake"))
+    timerId = setInterval(move , intervalTIme)
+
+}
+
+
+
+function move() {
+    if (
+        (currentSnake[0] + width >= width*width && direction === width) ||
+        (currentSnake[0] % width === width-1 && direction === 1) ||
+        (currentSnake[0]% width ===0 && direction ===-1)||
+        (currentSnake[0] - width < 0 && direction === -width)||
+          squares[currentSnake[0] + direction].classList.contains('snake')
+
+    )
+    return clearInterval(timerId)
+
+
+    const tail = currentSnake.pop()
+   squares[tail].classList.remove("snake")
+   currentSnake.unshift(currentSnake[0]+ direction)
+   squares[currentSnake[0]].classList.add("snake")
+
+
+   if(squares[currentSnake[0]].classList.contains("apple")){
+       squares[currentSnake[0]].classList.remove("apple")
+       squares[tail].classList.add("snake")
+       currentSnake.push(tail)
+       console.log(currentSnake)
+       generateApples()
+       score++
+       scoreDisplay.textContent = score 
+       clearInterval(timerId)
+       intervalTIme = intervalTIme * 0.9
+       timerId = setInterval (move, intervalTIme)
+
+       
+   }
+
+}
+
+
+
+function generateApples(){
+    do{
+       appleIndex = Math.floor(Math.random()* squares.length)
+    }while(squares[appleIndex].classList.contains("snake"))
+    squares[appleIndex].classList.add("apple")
+}
+generateApples()
+
+
+// 39 is right arrow
+// 38 is for the up arrow
+// 37 is for the left arrow
+// 40 is for the down arrow
+
+
+
+function control(e) {
+    if (e.keyCode === 39) {
+        console.log('right pressed')
+        direction = 1
+    } else if (e.keyCode === 38) {
+        console.log('up pressed')
+        direction = -width
+    } else if (e.keyCode === 37) {
+        console.log('left pressed')
+        direction = -1
+    } else if (e.keyCode === 40) {
+        console.log('down pressed')
+        direction = +width
+    }
+}
+
+
+
+
+document.addEventListener('keyup', control)
+
+button.addEventListener('click', startButton)
